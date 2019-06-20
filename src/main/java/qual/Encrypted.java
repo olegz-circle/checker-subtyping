9d468a0df54d3710115a9d0c11e0165d3e2b0667
Oleg Zenchenko <ozenchenko@circle.com>
2019-06-20 16:47:24 -0400

    Attempt to adopt subtyping example to maven

---
+++ src/main/java/qual/Encrypted.java
@@ -0,0 +1,12 @@
+package qual;
+
+
+import org.checkerframework.framework.qual.SubtypeOf;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@SubtypeOf(PossiblyUnencrypted.class)
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+public @interface Encrypted {
+}
---
+++ src/main/java/qual/PolyEncrypted.java
@@ -0,0 +1,11 @@
+package qual;
+
+import org.checkerframework.framework.qual.PolymorphicQualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@PolymorphicQualifier
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+public @interface PolyEncrypted {
+}
